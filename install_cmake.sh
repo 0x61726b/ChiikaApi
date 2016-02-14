ac5036c617c867ef6a5ecd19759cf905de25e857
arkenthera <arkengraphics@gmail.com>
2016-02-14 20:29:55 +0200

    travis

--- install_cmake.sh
+++ install_cmake.sh
@@ -4,3 +4,6 @@ wget --no-check-certificate http://cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64
 tar -xzvf cmake-3.2.2-Linux-x86_64.tar.gz
 export PATH=$(pwd)/cmake-3.2.2-Linux-x86_64/bin:$PATH
 cmake --version
+mkdir build
+cd build
+cmake ..
